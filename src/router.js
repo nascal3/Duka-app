3eec70de0310daaf6d4bc92abf63cd4219b25a3c
nascal3 <nascal3@gmail.com>
2018-08-28 01:48:49 +0300

    removed referensses to signin route

--- src/router.js
+++ src/router.js
@@ -4,14 +4,12 @@ import store from './store'
 
 import WelcomePage from './components/welcome/welcome.vue'
 import DashboardPage from './components/dashboard/dashboard.vue'
-import SignupPage from './components/auth/signup.vue'
 import SigninPage from './components/auth/signin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: WelcomePage },
-  { path: '/signup', component: SignupPage },
   { path: '/signin', component: SigninPage },
   {
     path: '/dashboard', component: DashboardPage,
